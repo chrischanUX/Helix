8db9313ea3cecdbec0b5cf66b556ad607f62ec97
Chris Chan <cchan@cchan-MBP.local>
2018-07-28 17:19:43 -0700

    Link text

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Syapse Helix Design System',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Welcome to Helix</h1>
+    <p>The latest design system used by the Syapse suite of products.</p>
+    <Link to="/page-2/">View All Components</Link>
+  </div>
+)
+
+export default IndexPage
